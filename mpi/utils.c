b833f090899eb1619ec1584e403c890c46e2f02d
Alessandro Lioi <lioialessandro2000@live.com>
2024-03-18 18:35:36 +0100

    chore(mpi): comments

--- mpi/utils.c
+++ mpi/utils.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Return parsed int from arg; exits otherwise
 int parse_int_arg(char *arg) {
   int val = strtol(arg, NULL, 10);
   if ((val == INT_MAX || val == INT_MIN) && errno == ERANGE) {
@@ -13,9 +14,10 @@ int parse_int_arg(char *arg) {
   return val;
 }
 
+// Calculate start and end based on coord
 void calculate_start_end(int size, int dims, int coord, int offset, int *start, int *end) {
   int block_size = size / dims;
-  int rest = size % dims;
+  int rest = size % dims; // Used to fairly distribute work
   *start = (offset + coord) * block_size % size;
   *end = 0;
   if (coord < rest) {
