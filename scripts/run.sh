cba529725dd21e961bb6dc4ec0460842978ae8d6
Alessandro Lioi <lioialessandro2000@live.com>
2024-04-03 12:25:08 +0200

    docs(scripts): better prints on run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# $1: execution type: mpi or omp or mpi-omp
+# $1: calculation type: mpi or omp or mpi-omp
 # $2: generation type: random or index
 
 if ! [ -f ./build/scpa-matrix-generator ] || ! [ -f ./build/mpi/scpa-mpi ] || ! [ -f ./build/mpi/scpa-mpi-omp ] || ! [ -f ./build/omp/scpa-omp ]; then
     echo "The project was not built correctly. Building now..."
-    module load mpich # or module load mpi in local development environment
+    module load gnu mpich # or module load mpi in local development environment
     cmake -B build && cmake --build build
 fi
 
@@ -16,16 +16,20 @@ p_vals=(2 4 8 16 24 32)
 # Syntax: mpi_run <m> <n> <k>
 mpi_run() {
     for p in "${p_vals[@]}"; do
-        echo -e "\t\tNumber of Processes: $p"
+        echo -e "\tNumber of Processes: $p (m: ${1}, n: ${2}, k: ${3})"
         mpirun -n $p ./build/mpi/scpa-mpi $1 $2 $3
     done
 }
 
 # Syntax: mpi_omp_run <m> <n> <k>
 mpi_omp_run() {
-    for p in "${p_vals[@]}"; do
-        for t in "${p_vals[@]}"; do
-            echo -e "\t\tNumber of Processes: $p; Number of Threads: $t"
+    length=${#p_vals[@]}
+    for ((i = 0; i < length; i++)); do
+        p=${p_vals[i]}
+        for ((j = i + 1; j < length; j++)); do
+            t=${p_vals[j]}
+            x=$((p * t))
+            echo -e "\tNumber of Processes: $p; Number of Threads: $t (m: ${1}, n: ${2}, k: ${3}; x = ${x})"
             mpirun -n $p ./build/mpi/scpa-mpi-omp $1 $2 $3 $t
         done
     done
@@ -34,7 +38,7 @@ mpi_omp_run() {
 # Syntax: omp_run <m> <n> <k>
 omp_run() {
     for t in "${p_vals[@]}"; do
-        echo -e "\t\tNumber of Threads: $t"
+        echo -e "\tNumber of Threads: $t (m: ${1}, n: ${2}, k: ${3})"
         ./build/omp/scpa-omp $1 $2 $3 $t
     done
 }
@@ -54,12 +58,11 @@ run() {
     fi
 }
 
-# Syntax: main[ <type> <gen_type>]
+# Syntax: main[ <calc_type> <gen_type>]
 main() {
     echo "Square Matrices"
     for m in "${size_vals[@]}"; do
-        echo -e "\tRunning size $m $m $m"
-        echo -e "\t\tGenerating Matrix"
+        echo -e "\tGenerating Matrix (size: ${m})"
         ./build/scpa-matrix-generator $m $m $m $2
         run $1 $m $m $m
     done
@@ -68,8 +71,7 @@ main() {
         for n in "${size_vals[@]}"; do
             for k in "${k_vals[@]}"; do
                 if [[ m -ne n ]] && [[ m -ne n ]] && [[ n -ne k ]]; then 
-                    echo -e "\tRunning size $m $n $k"
-                    echo -e "\t\tGenerating Matrix"
+                    echo -e "\tGenerating Matrix (m: ${m}, n: ${n}, k: ${k})"
                     ./build/scpa-matrix-generator $m $n $k $2
                     run $1 $m $n $k
                 fi
