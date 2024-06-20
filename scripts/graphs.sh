45b8fa5663c10bc403135723e6855b9862ded64e
Alessandro Lioi <lioialessandro2000@live.com>
2024-06-20 16:42:29 +0200

    fix(scripts): graphs check venv folder; env actiation

--- scripts/graphs.sh
+++ scripts/graphs.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if ! [ -d .venv] ; then
+if ! [ -d .venv ] ; then
     echo "Creating Python Virtual Environment"
     python3 -m venv .venv > /dev/null
 
-    echo "Activating Python Virtual Environment"
-    source .venv/bin/activate > /dev/null
 
     echo "Installing packages"
     pip install -r requirements.txt > /dev/null
 fi
 
+echo "Activating Python Virtual Environment"
+source .venv/bin/activate > /dev/null
+
 echo "Creating graphs"
 python3 src/graphs/main.py
 
-echo "Cleaning up"
+echo "Deactivating Python Virtual Environment"
 deactivate > /dev/null
