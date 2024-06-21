08a595761277bd5735cfcfc1642fb632c0fc5606
Alessandro Lioi <lioialessandro2000@live.com>
2024-06-21 11:26:16 +0200

    fix(scripts): clean command

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,7 +2,7 @@
 
 # Remove temp folders
 rm -rf build .cache .venv 2&> /dev/null
-find output ! -name "*.csv" -delete
+find output ! -name "*.csv" -type f -delete -print
 
 # Remove valgrind output
 rm valgrind-out* callgrind.* 2&> /dev/null
