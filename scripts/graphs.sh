752861d2b589af8b708b2eb40f87b1ed7fe96261
Alessandro Lioi <lioialessandro2000@live.com>
2024-06-22 11:30:31 +0200

    fix: removed png from gitignore; fix graphs script

--- scripts/graphs.sh
+++ scripts/graphs.sh
@@ -4,6 +4,8 @@ if ! [ -d .venv ] ; then
     echo "Creating Python Virtual Environment"
     python3 -m venv .venv > /dev/null
 
+    echo "Activating Python Virtual Environment"
+    source .venv/bin/activate > /dev/null
 
     echo "Installing packages"
     pip install -r requirements.txt > /dev/null
